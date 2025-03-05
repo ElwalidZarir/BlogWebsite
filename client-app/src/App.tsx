ae96115bf8328289512ae8fddfe5006eb5957063
elwalidzarir <wazardo99@gmail.com>
2025-03-05 16:38:52 +0100

    setup structure layout and responsive navbar

---
+++ client-app/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <NavBar />
+      {/* BreadCRUMB */}
+      {/* Introduction */}
+      {/* Featured posts */}
+      {/* Posts Lists */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client-app/src/components/NavBar.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* Logo */}
+      <div className="flex gap-4 items-center text-2xl font-bold">
+        <img src={logo} className="w-8 h-8" alt="" />
+        <span>EducBlog</span>
+      </div>
+      {/* Mobile Menu */}
+      <div className="md:hidden">
+        <div className="cursor-pointer" onClick={() => setOpen(!open)}>
+          {open ? "X" : "☰"}
+        </div>
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="">Trending</a>
+          <a href="">Most Popular</a>
+          <a href="">About</a>
+          <a href="">
+            <button className="py-2  px-2 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* Desktop Menu */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="">Trending</a>
+        <a href="">Most Popular</a>
+        <a href="">About</a>
+        <a href="">
+          <button className="py-2  px-2 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
